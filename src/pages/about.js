7191ae6bc1b371bf6ad994ac4538b888693d15f8
Dylan Stratton <dm.stratton94@gmail.com>
2018-02-09 07:57:49 -0600

    add about links

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,10 +12,10 @@ const About = () => (
         <b>Jack Rossiter-Munley</b> is a paralegal by day and a freelance
         writer/editor/podcaster by night. In addition to co-hosting Close
         Talking, he is also the editor-in-chief for{' '}
-        <a href="#">trolltennis.com</a> and the host of the New Books in
+        <a target="_blank" rel="noopener" href="http://trolltennis.com/">trolltennis.com</a> and the host of the New Books in
         National Security podcast. Jack produces Poetry Spoken Here, a poetry
         interview podcast. Jack also co-hosts a Shakespeare podcast,{' '}
-        <a href="#">Party Bard</a>, with YA author Molly Booth. The music heard
+        <a target="_blank" rel="noopener" href="https://itunes.apple.com/us/podcast/party-bard/id1263721147?mt=2">Party Bard</a>, with YA author Molly Booth. The music heard
         at the beginning and end of every Close Talking episode is composed and
         performed by Jack.
       </p>
