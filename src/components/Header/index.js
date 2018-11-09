fb63c902093706bd2065db6fc77f996f3bef444a
Dylan Stratton <dm.stratton94@gmail.com>
2018-11-08 22:36:37 -0600

    update logo image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import logo from '../../assets/images/ctlogo-updated3.png';
+import logo from '../../assets/images/ct-logo-v4.png';
 
 const Header = () => (
   <div className="nav">
