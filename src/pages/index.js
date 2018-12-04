efc95ceb0572b23011960c35a066be3414f45a3a
Dylan Stratton <dm.stratton94@gmail.com>
2018-12-03 21:17:40 -0600

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Link from 'gatsby-link';
-import { Socials } from '../components/Socials';
-import logo from '../assets/images/ctlogo-updated3-noImage.png';
+import logo from '../assets/images/ct-logo-v4-no-image.png';
 
 class IndexPage extends Component {
 
@@ -15,26 +14,26 @@ class IndexPage extends Component {
     return (
       <section id="home" className="landing">
         <div className="landing__heading">
-          {/* <Socials podcasts /> */}
-          <div className="landing__nav">
-            <div>
-              <Link className="landing__nav--item" to="/podcasts/">Podcasts</Link>
-              <Link className="landing__nav--item" to="/about/">About</Link>
-            </div>
-            <img className="landing__logo" src={logo} alt="Close Talking: A Poetry Podcast" />
-          </div>
-          <p className="landing__heading--text">
-            Close Talking is a podcast hosted by good friends Connor Stratton and
-            Jack Rossiter-Munley. In each episode the two read a poem and discuss
-            at length. The pop culture references fly as freely as the literary
-            theories. Close Talking is a poetry podcast anyone can enjoy.
-          </p>
+          <img className="landing__logo" src={logo} alt="Close Talking: A Poetry Podcast" />
         </div>
-        {/* <div className="podcast landing__podcast"> */}
-        <Link className="landing__recent" to={`/podcasts/${recent.node.title.replace(/\s+/g, '-').toLowerCase()}/`}>
-          <h1 className="podcast__title">{recent.node.title}</h1>
-        </Link>
-        {/* <p className="podcast__date">{recent.node.date}</p>
+
+        <div className="landing__nav">
+          <Link className="landing__nav--item" to="/podcasts/">Podcasts</Link>
+          <Link className="landing__nav--item" to="/about/">About</Link>
+        </div>
+
+        <p className="landing__heading--text">
+          Close Talking is a podcast hosted by good friends Connor Stratton and
+          Jack Rossiter-Munley. In each episode the two read a poem and discuss
+          at length. The pop culture references fly as freely as the literary
+          theories. Close Talking is a poetry podcast anyone can enjoy.
+        </p>
+
+        <div className="podcast landing__podcast">
+          <Link className="landing__recent" to={`/podcasts/${recent.node.title.replace(/\s+/g, '-').toLowerCase()}/`}>
+            <h1 className="podcast__title">{recent.node.title}</h1>
+          </Link>
+          <p className="podcast__date">{recent.node.date}</p>
           {recent.node.soundcloudLink ?
             <div className="podcast__audio" dangerouslySetInnerHTML={{ __html: recent.node.soundcloudLink.soundcloudLink }} /> :
             <div className="podcast__no-audio">No SoundCloud Player available.</div>
@@ -44,8 +43,8 @@ class IndexPage extends Component {
             dangerouslySetInnerHTML={{
               __html: recent.node.content.childMarkdownRemark.html
             }}
-          /> */}
-        {/* </div> */}
+          />
+        </div>
       </section>
     );
   }
